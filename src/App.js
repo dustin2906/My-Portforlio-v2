25673c291785f7eebae76447f9264158b219cc39
= <=>
2020-10-21 21:24:38 +0300

    complete my carousal page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from './components/my-carousal/my-carousal.component';
+import TitleMessage from './components/titleMessage/titlemessage.component';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MyNavbar />
+    <MyCarousal />
+    <TitleMessage />
     </div>
   );
 }
---
+++ src/components/my-navbar/mynavbar.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Logo from '../../assets/icons/logo.png';
+import './mynavbar.style.css'
+
+const MyNavbar = () => {
+  return (
+    <div>
+    <Navbar collapseOnSelect 
+    expand="md" 
+    variant="dark"
+    fixed="top"
+    expand="md"
+    className="animate-navbar nav-theme justify-content-between">
+    <Navbar.Brand href="#come">
+    <img className="logo" src={Logo} alt="my logo"/>
+    
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#about">About</Nav.Link>
+        <Nav.Link href="#skill">Skill</Nav.Link>
+        <Nav.Link href="#experiences">Experiences</Nav.Link>
+        <Nav.Link href="#project">Project</Nav.Link>
+        <Nav.Link href="#contact">Contact</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+    </div>
+  )
+}
+
+
+export default MyNavbar;
