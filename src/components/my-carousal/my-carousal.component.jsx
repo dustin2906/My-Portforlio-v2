b0a691ec92c41b62ed91e8a11d8ae21ab72f316e
= <=>
2020-10-24 11:01:27 +0300

    fix carousel

--- src/components/my-carousal/my-carousal.component.jsx
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import Slide1 from '../../assets/img/carousal/slide1.webp';
-import Slide2 from '../../assets/img/carousal/slide2.webp';
-import Slide3 from '../../assets/img/carousal/slide3.webp';
+import Slide1 from '../../assets/img/carousal/first.jpg';
+import Slide2 from '../../assets/img/carousal/second.jpg';
+import Slide3 from '../../assets/img/carousal/third.jpg';
 import ScrollDown from '../ScrollDown/scrollDown.component';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './my-carousal.style.css';
@@ -10,11 +10,11 @@ import './my-carousal.style.css';
 
 const MyCarousal = () => {
   return (
-    <div id="home" lg={12}>
-      <Carousel controls={false} interval={2500} pause={false} no-gutters >
+    <div id="home">
+      <Carousel controls={false} interval={2500} pause={false} >
         <Carousel.Item >
           <img
-            className=" w-100 custom-img"
+            className="d-block w-100 custom-img"
             src={Slide1}
             alt="First slide"
           />
@@ -22,7 +22,7 @@ const MyCarousal = () => {
 
         <Carousel.Item>
           <img
-            className=" w-100 custom-img"
+            className="d-block w-100 custom-img"
             src={Slide2}
             alt="Third slide"
           />
@@ -30,7 +30,7 @@ const MyCarousal = () => {
         
         <Carousel.Item>
           <img
-            className="w-100 custom-img"
+            className="d-block w-100 custom-img"
             src={Slide3}
             alt="Third slide"
           />
