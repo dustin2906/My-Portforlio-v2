7841e47123760cc3c8c36393bafdd4941d89492a
thoarrown <thoarrown@gmail.com>
2020-11-03 13:50:44 +0700

    Fix url

---
+++ build/precache-manifest.7574f329608c05aa39f8c4a1067ad3bb.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ee2cb18c1e75457efd88295d5d97c3",
+    "url": "/My-Portforlio-v2/index.html"
+  },
+  {
+    "revision": "a159c8c5f2c2f2ad3740",
+    "url": "/My-Portforlio-v2/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "59537d4d4bdf432cfbf5",
+    "url": "/My-Portforlio-v2/static/css/main.2da107f2.chunk.css"
+  },
+  {
+    "revision": "a159c8c5f2c2f2ad3740",
+    "url": "/My-Portforlio-v2/static/js/2.0060ec69.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/My-Portforlio-v2/static/js/2.0060ec69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59537d4d4bdf432cfbf5",
+    "url": "/My-Portforlio-v2/static/js/main.2d3961ee.chunk.js"
+  },
+  {
+    "revision": "9b2057a8bb0cad304c91",
+    "url": "/My-Portforlio-v2/static/js/runtime-main.b265631b.js"
+  },
+  {
+    "revision": "933989a33a2f4b99f210a509ce6427cf",
+    "url": "/My-Portforlio-v2/static/media/Mayvary.933989a3.png"
+  },
+  {
+    "revision": "10746b6a7c8e075a9cb9646ef6413007",
+    "url": "/My-Portforlio-v2/static/media/TODOLIST.10746b6a.png"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/My-Portforlio-v2/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/My-Portforlio-v2/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "c3ce0c15c4fa916c837e84474bf143d1",
+    "url": "/My-Portforlio-v2/static/media/css3-icon.c3ce0c15.png"
+  },
+  {
+    "revision": "c90f249d46efe36c331b8e169c1b9cf9",
+    "url": "/My-Portforlio-v2/static/media/data1.c90f249d.png"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/My-Portforlio-v2/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/My-Portforlio-v2/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "567f006b9ca974f343db1c9c79920074",
+    "url": "/My-Portforlio-v2/static/media/github.567f006b.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/My-Portforlio-v2/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "0a6589ac6cd8fa39ca06e37c1b5a6ec0",
+    "url": "/My-Portforlio-v2/static/media/html5-icon.0a6589ac.png"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/My-Portforlio-v2/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "b64d6e6589e632d3cbc7ca3a15dedf8f",
+    "url": "/My-Portforlio-v2/static/media/logo.b64d6e65.png"
+  },
+  {
+    "revision": "589bd369001347f9ed31c5c47346c33c",
+    "url": "/My-Portforlio-v2/static/media/mayvarylogo.589bd369.png"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/My-Portforlio-v2/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "8e45dd155eeb67a6e6f77f4d6a397f83",
+    "url": "/My-Portforlio-v2/static/media/myportfoliov1.8e45dd15.png"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/My-Portforlio-v2/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "a841fe945f24610f9fd177f98872aea9",
+    "url": "/My-Portforlio-v2/static/media/php.a841fe94.png"
+  },
+  {
+    "revision": "24e4a836a5f1af5aa65424a447e97c6e",
+    "url": "/My-Portforlio-v2/static/media/profilepic.24e4a836.png"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/My-Portforlio-v2/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/My-Portforlio-v2/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/My-Portforlio-v2/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/My-Portforlio-v2/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/My-Portforlio-v2/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "4b50e9565d84c4712f3e9e4ab1737e97",
+    "url": "/My-Portforlio-v2/static/media/reviewmovie.4b50e956.png"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/My-Portforlio-v2/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/My-Portforlio-v2/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/My-Portforlio-v2/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "f4be0cc0f6dae8a6151deff12c54d6bd",
+    "url": "/My-Portforlio-v2/static/media/wordpress-icon.f4be0cc0.png"
+  }
+]);
